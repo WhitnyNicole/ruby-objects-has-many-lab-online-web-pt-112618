39d0b46f210adf2b36e7503e9eeeebfc14e4862e
Whitny Edwards <wne4e2@gmail.com>
2019-01-12 20:56:17 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,17 @@
+  def add_song(song)
+    @songs << song
+  end 
+
+  def songs
+    @songs
+  end 
+  
+  def add_song_by_name(name, genre)
+    song = Song.new(name, genre)
+    @songs << song
+    song.artist = self
+  end 
+
+  def artist_name
+    self.artist.name
+  end 
